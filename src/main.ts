6c6520c3c8eace18c90b8cd32ce2724c54c958c4
„franzdmitrij“ <franz.dmitrij@gmail.com>
2024-06-05 13:50:24 +0200

    Typescript

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Test, x } from './test';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
-  class Test{
-    constructor(public zahl: number){
-      console.log("test");
-    }
-  }
-
 const test = new Test(42);
+console.log(test.zahl);
+console.log(x);
---
+++ src/test.ts
@@ -0,0 +1,8 @@
+
+export const x = 3;
+
+export class Test{
+  constructor(public zahl: number){
+    console.log("test");
+  }
+}
