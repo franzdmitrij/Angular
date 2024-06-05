6cc60ab0d20419ceb8fa0ade9e2aa22e608de930
„franzdmitrij“ <franz.dmitrij@gmail.com>
2024-06-05 14:27:23 +0200

    Alter neuer Stil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,5 +9,19 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'book-rating';
+
+  // Alter Stil
+  title = 'Book Rating';
+  zahl = 100;
+
+  title2 = signal('Book Rating 2')
+
+
+
+  constructor(){
+    window.setTimeout(() => this.title = "bbblubbb", 1000);
+
+    window.setTimeout(() => this.title2.set("bbblubbb 2"), 1000);
+
+  }
 }
