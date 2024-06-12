a0621a102fb40b3607e09c17a5ba28e789e34b62
„franzdmitrij“ <franz.dmitrij@gmail.com>
2024-06-12 15:10:19 +0200

    ngrx

--- src/app/books/store/book.effects.ts
+++ src/app/books/store/book.effects.ts
@@ -3,6 +3,7 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { catchError, map, concatMap } from 'rxjs/operators';
 import { Observable, EMPTY, of } from 'rxjs';
 import { BookActions } from './book.actions';
+import { BookStoreService } from '../shared/book-store.service';
 
 
 @Injectable()
@@ -10,13 +11,15 @@ export class BookEffects {
 
   actions$ = inject(Actions);
 
+  bookStore = inject(BookStoreService);
+
   loadBooks$ = createEffect(() => {
     return this.actions$.pipe(
 
       ofType(BookActions.loadBooks),
       concatMap(() =>
         /** An EMPTY observable only emits completion. Replace with your own observable API request */
-        EMPTY.pipe(
+        this.bookStore.getAllBooks().pipe(
           map(books => BookActions.loadBooksSuccess({ books })),
           catchError(error => of(BookActions.loadBooksFailure({ error }))))
       )
